53f1f4163199d5ed0146d91f4fc28b887cbf624f
jyzeng17 <jyzeng17@gmail.com>
2018-10-21 15:20:20 +0800

    Add missing Tagbar dependency and modify runtime info

--- install.sh
+++ install.sh
@@ -1,73 +1,104 @@
 #!/bin/bash
 
-# Check arguments
-if [ "$#" -gt 1 ]; then
-	echo "[Info] Usage: ./install.sh [-f]"
-	echo "[Info] Arguments: -f: Ignore existed backup directory."
-	echo "[Error] Invalid arguments."
-	exit 1
-elif [ "$#" -eq 1 ] && [ "${1}" != "-f" ]; then
-	echo "[Info] Usage: ./install.sh [-f]"
-	echo "[Info] Arguments: -f: Ignore existed backup directory."
-	echo "[Error] Invalid arguments."
+if [ "$#" -gt 0 ]; then
+	echo "Usage: ./install.sh"
 	exit 1
 fi
 
-echo -n "[Info] Checking git...... "
+echo -n "checking for git... "
 if ! [ -x "$(command -v git)" ]; then
+	echo "no"
 	sudo apt-get install -y git
-	echo "installed."
 else
-	echo "already installed."
+	echo "yes"
 fi
 
-echo "[Info] Checking Neovim...... "
+echo -n "checking for Neovim... "
 if ! [ -x "$(command -v nvim)" ]; then
-	echo -n "[Info] Checking apt-add-repository...... "
+	echo "no"
+	echo -n "checking for apt-add-repository... "
 	if ! [ -x "$(command -v apt-add-repository)" ]; then
+		echo "no"
 		sudo apt-get install -y software-properties-common
-		echo "installed."
 	else
-		echo "already installed."
+		echo "yes"
 	fi
 	sudo apt-add-repository -y ppa:neovim-ppa/stable
 	sudo apt-get update
 	sudo apt-get install -y neovim
 	sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-	echo "[Info] ...... Neovim installed."
 else
-	echo "[Info] ...... Neovim already installed."
+	echo "yes"
 fi
 
-echo -n "[info] Checking Vundle...... "
+echo -n "checking for Vundle... "
 if ! [ -d ~/.vim/bundle/Vundle.vim ]; then
+	echo "no"
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	echo "installed."
 else
-	echo "already installed."
+	echo "yes"
 fi
 
-echo -n "[Info] Checking backup directory...... "
-if [ -d ~/.config/nvim_backup ] && [ "$#" -eq 0 ]; then
-	echo -e "\n[Error] \"~/.config/nvim_backup\" already exists. Change its name or move it somewhere else. Or run the script with -f argument to ignore this error."
-	exit 1
-else
-	echo "checked."
-fi
-
-echo -n "[Info] Checking nvim directory...... "
+echo -n -e "checking if \"~/.config/nvim\" exists... "
 if [ -d ~/.config/nvim ]; then
-	cp -r ~/.config/nvim ~/.config/nvim_backup
-	echo "old directory's been copied to backup directory."
+	echo "yes"
+	echo -n -e "checking if \"~/.config/nvim_backup\" exists... "
+	if [ -d ~/.config/nvim_backup ] && [ "$#" -eq 0 ]; then
+		echo -n "yes. "
+		read -p "Do you want to override it? [y/n] " answer
+		until [ "$answer" == "y" ] || [ "$answer" == "Y" ] || [ "$answer" == "n" ] || [ "$answer" == "N" ]
+		do
+			read -p "Invalid answer. Do you want to override the existed backup directory? [y/n] " answer
+		done
+		if [ "$answer" == "y" ] || [ "$answer" == "Y" ]; then
+			cp -r ~/.config/nvim ~/.config/nvim_backup
+		else
+			echo "Aborted."
+			exit 1
+		fi
+	else
+		echo "no"
+		cp -r ~/.config/nvim ~/.config/nvim_backup
+	fi
 else
+	echo "no"
 	mkdir -p ~/.config/nvim
-	echo "generated."
+fi
+
+# Tagbar's dependency
+echo -n "checking for ctags... "
+if ! [ -x "$(command -v ctags)" ]; then
+	echo "no"
+	git clone https://github.com/universal-ctags/ctags.git
+	echo -n "checking for autoreconf... "
+	if ! [ -x "$(command -v autoreconf)" ]; then
+		echo "no"
+		sudo apt-get -y install dh-autoreconf
+	else
+		echo "yes"
+	fi
+	echo -n "checking for pkg-config... "
+	if ! [ -x "$(command -v pkg-config)" ]; then
+		echo "no"
+		sudo apt-get -y install pkg-config
+	else
+		echo "yes"
+	fi
+	ctags/autogen.sh
+	ctags/configure
+	make
+	sudo make install
+else
+	echo "yes"
 fi
 
 cp basic/* ~/.config/nvim
-echo -n "[Info] Switch to Neovim to install plugins...... "
+
+echo -n "switching to Neovim to install plugins... "
 nvim +PluginInstall +qall
-echo "done."
+echo "done"
+
 cp plugin/init_plugins.vim ~/.config/nvim
-echo "[Info] Successfully completed!"
+
+echo "successfully completed!"
 exit 0
