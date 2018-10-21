d8ccdac63559c9aa7978fc739da35616c72ea3bc
jyzeng17 <jyzeng17@gmail.com>
2018-10-21 12:19:27 +0800

    Improve runtime info and add argument checking in install.sh

--- install.sh
+++ install.sh
@@ -1,58 +1,73 @@
 #!/bin/bash
 
-echo "[info] checking git......"
-if ! [ -x "$(command -v git)" ]; then
-	sudo apt-get install -y git
-	echo "[info] git installed"
-else
-	echo "[info] git already installed"
+# Check arguments
+if [ "$#" -gt 1 ]; then
+	echo "[Info] Usage: ./install.sh [-f]"
+	echo "[Info] Arguments: -f: Ignore existed backup directory."
+	echo "[Error] Invalid arguments."
+	exit 1
+elif [ "$#" -eq 1 ] && [ "${1}" != "-f" ]; then
+	echo "[Info] Usage: ./install.sh [-f]"
+	echo "[Info] Arguments: -f: Ignore existed backup directory."
+	echo "[Error] Invalid arguments."
+	exit 1
 fi
 
-echo "[info] checking Neovim......"
+echo -n "[Info] Checking git...... "
+if ! [ -x "$(command -v git)" ]; then
+	sudo apt-get install -y git
+	echo "installed."
+else
+	echo "already installed."
+fi
+
+echo "[Info] Checking Neovim...... "
 if ! [ -x "$(command -v nvim)" ]; then
-	echo "[info] checking apt-add-repository......"
+	echo -n "[Info] Checking apt-add-repository...... "
 	if ! [ -x "$(command -v apt-add-repository)" ]; then
 		sudo apt-get install -y software-properties-common
-		echo "[info] apt-add-repository installed"
+		echo "installed."
 	else
-		echo "[info] apt-add-repository already installed"
+		echo "already installed."
 	fi
 	sudo apt-add-repository -y ppa:neovim-ppa/stable
 	sudo apt-get update
 	sudo apt-get install -y neovim
 	sudo apt-get install -y python-dev python-pip python3-dev python3-pip
-	echo "[info] Neovim installed"
+	echo "[Info] ...... Neovim installed."
 else
-	echo "[info] Neovim already installed"
+	echo "[Info] ...... Neovim already installed."
 fi
 
-echo "[info] checking Vundle......"
+echo -n "[info] Checking Vundle...... "
 if ! [ -d ~/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	echo "[info] Vundle installed"
+	echo "installed."
 else
-	echo "[info] Vundle already installed"
+	echo "already installed."
 fi
 
-echo "[info] checking backup directory......"
-if [ -d ~/.config/nvim_backup ]; then
-	echo "[error] \"~/.config/nvim_backup\" already exists. Change its name or move it somewhere else and run the script again."
+echo -n "[Info] Checking backup directory...... "
+if [ -d ~/.config/nvim_backup ] && [ "$#" -eq 0 ]; then
+	echo -e "\n[Error] \"~/.config/nvim_backup\" already exists. Change its name or move it somewhere else. Or run the script with -f argument to ignore this error."
 	exit 1
 else
-	echo "[info] backup directory checked"
+	echo "checked."
 fi
 
-echo "[info] checking nvim directory......"
+echo -n "[Info] Checking nvim directory...... "
 if [ -d ~/.config/nvim ]; then
 	cp -r ~/.config/nvim ~/.config/nvim_backup
-	echo "[info] old nvim directory's been copied to nvim_backup directory for recovery"
+	echo "old directory's been copied to backup directory."
 else
 	mkdir -p ~/.config/nvim
-	echo "[info] nvim directory generated"
+	echo "generated."
 fi
 
 cp basic/* ~/.config/nvim
+echo -n "[Info] Switch to Neovim to install plugins...... "
 nvim +PluginInstall +qall
+echo "done."
 cp plugin/init_plugins.vim ~/.config/nvim
-echo "[info] successfully completed!"
+echo "[Info] Successfully completed!"
 exit 0
